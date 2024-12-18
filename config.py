ccaa439b0ee81ba2a1c6b1ade90a3ef41c91af55
Shina Cao <142919735+SheenaCao@users.noreply.github.com>
2024-12-18 01:37:03 -0600

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,10 @@
 import os
 
 class Config:
-    # Use Windows Authentication
-    SQLALCHEMY_DATABASE_URI = 'mssql+pyodbc://localhost\\SQLEXPRESS/Care_Connect?driver=ODBC+Driver+17+for+SQL+Server'
+    # Azure SQL Database connection string
+    SQLALCHEMY_DATABASE_URI = (
+        "mssql+pyodbc://sheenacao:c19990403c@sheenacao.database.windows.net:1433/Care_Connect"
+        "?driver=ODBC+Driver+17+for+SQL+Server"
+    )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = os.urandom(24) 
+    SECRET_KEY = os.urandom(24)
